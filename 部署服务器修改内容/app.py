c942616eec75a5d442b4a0142480e69e52bf5df3
Zhang Kun <2325931355@qq.com>
2026-03-10 13:27:56 +0800

    Update app.py

--- 部署服务器修改内容/app.py
+++ 部署服务器修改内容/app.py
@@ -57,7 +57,7 @@ PASSWORD_REGEX = re.compile(r'^[a-zA-Z0-9!@#$%^&*()_\-+=\[\]{}|\\;:\'",.<>/?]+$'
 EMAIL_REGEX = re.compile(r'^[^\s@]+@[^\s@]+\.[^\s@]+$')
 
 # 管理员密钥（应该从环境变量或配置文件读取）
-ADMIN_KEY = "Zhangkun*0914"  # 请修改为你的管理员密钥
+ADMIN_KEY = "123456"  # 请修改为你的管理员密钥
 
 
 def login_required(required_type=None):
